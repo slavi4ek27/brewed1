af76113fb4057eeab065427e39bd2c584ce42963
slavi4ek27 <slavi4ek27@gmail.com>
2016-03-25 14:34:55 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.bxslider').bxSlider({
+		nextSelector: '#slider-next',
+		prevSelector: '#slider-prev',
+		nextText: 'Onward →',
+		prevText: '← Go back'
+	});
+});
